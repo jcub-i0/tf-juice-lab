e538740456ae8d474185b4dcd9fc7ffbaeeaa820
jcub <jcub@jcub.jcub>
2025-06-03 18:40:24 -0400

    Created Public and Private route table associations with the corresponding subnets

--- main.tf
+++ main.tf
@@ -197,3 +197,13 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.igw.id
   }
 }
+
+resource "aws_route_table_association" "private_assc" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public_assc" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
