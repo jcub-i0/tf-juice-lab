e4fe8b8e8637baf165b84a0c7d8f60ae5632c51e
jcub <jcub@jcub.jcub>
2025-06-05 22:57:59 -0400

    Added output block to return public IP of Bastion instance

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "current_region" {
   value = var.aws_region
 }
 
+output "bastion_private_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
 output "kali_private_ip" {
   value = aws_instance.kali.private_ip
 }
