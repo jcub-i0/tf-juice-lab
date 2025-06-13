563b120fe0d3a13df2445d1f298973e57112db41
jcub <jcub@jcub.jcub>
2025-06-12 22:54:43 -0400

    Moved IAM-related IaC to iam.tf

---
+++ iam.tf
@@ -0,0 +1,82 @@
+# IAM Data Blocks
+data "aws_iam_policy" "ssm_core" {
+  name = "AmazonSSMManagedInstanceCore"
+}
+
+# CREATE AND ATTACH IAM ROLES, INSTANCE PROFILES, ETC
+
+resource "aws_iam_role" "ssm_role" {
+  name = "EC2-SSM-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = data.aws_iam_policy.ssm_core.arn
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "EC2-SSM-Profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+# S3 Bucket Policies
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.centralized_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.centralized_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Sid = "AWSCloudTrailListBucket"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:ListBucket"
+        Resource = aws_s3_bucket.centralized_logs.arn
+        Condition = {
+          StringEquals = {
+            "s3:prefix" = "AWSLogs/${data.aws_caller_identity.current.account_id}/"
+          }
+        }
+      },
+      {
+        Sid = "AWSCloudTrailGetBucketAcl"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.centralized_logs.arn
+      }
+    ]
+  })
+}
