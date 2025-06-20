60fbc87ed9bf8a287c294715f4395bb24df6718d
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-06-20 18:06:37 -0400

    Fixed another .bucket suffix

--- main.tf
+++ main.tf
@@ -389,7 +389,7 @@ resource "aws_s3_bucket" "centralized_logs" {
 
 ## Enable SSE encryption on centralized_logs bucket
 resource "aws_s3_bucket_server_side_encryption_configuration" "logs_bucket_encrypt" {
-  bucket = aws_s3_bucket.centralized_logs.id
+  bucket = aws_s3_bucket.centralized_logs.bucket
 
   rule {
     apply_server_side_encryption_by_default {
