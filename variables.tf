57247868b658b1f23a24cc5372e125f1a67e5b64
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-06-23 17:26:32 -0400

    Created 'securityhub_standards' variable (consists of standards to plug into Security Hub)

--- variables.tf
+++ variables.tf
@@ -57,3 +57,14 @@ variable "guardduty_features" {
     "LAMBDA_NETWORK_LOGS"
   ]
 }
+
+variable "securityhub_standards" {
+  description = "Map of short-named Security Hub standards to their ARNs"
+  type = map(string)
+  default = {
+    aws_fsbp = "arn:${var.partition}:securityhub:${var.aws_region}::standards/aws-foundational-security-best-practices/v/1.0.0",
+    cis = "arn:${var.partition}:securityhub:${var.aws_region}::standards/cis-aws-foundations-benchmark/v/3.0.0",
+    nist_800 = "arn:${var.partition}:securityhub:${var.aws_region}::standards/nist-800-53/v/5.0.0",
+    pci_dss = "arn:${var.partition}:securityhub:${var.aws_region}::standards/pci-dss/v/3.2.1"
+  }
+}
