fd16ae011a1cc5ba66a643645e095c6408ad670a
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-07-01 20:26:00 -0400

    Updated name of ec2_isolate lambda func's archive resource to align with the name of its specific zip

--- monitoring.tf
+++ monitoring.tf
@@ -253,7 +253,7 @@ resource "aws_securityhub_standards_subscription" "standards" {
 }
 
 # Zip file containing Lambda function code
-data "archive_file" "lambda_zip" {
+data "archive_file" "lambda_ec2_isolate_zip" {
   type        = "zip"
   source_file = "${path.module}/lambda/ec2_isolate/ec2_isolate_function.py"
   output_path = "${path.module}/lambda/ec2_isolate/ec2_isolate_function.zip"
@@ -295,7 +295,7 @@ resource "aws_cloudwatch_event_target" "securityhub_ec2_isolate_target" {
 resource "aws_lambda_function" "ec2_isolation" {
   function_name = "ec2_isolation"
   description   = "Isolate compromised EC2 instance by placing it in Quarantine SG"
-  filename      = data.archive_file.lambda_zip.output_path
+  filename      = data.archive_file.lambda_ec2_isolate_zip.output_path
   handler       = "ec2_isolate_function.lambda_handler"
 
   runtime = "python3.12"
