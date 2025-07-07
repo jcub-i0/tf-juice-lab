a68f43d83aff83f2d198bbe9ba114c653cb23377
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-07-07 12:25:55 -0400

    Added 'event' and 'context' as arguments to lambda_handler() func

--- lambda/ec2_autostop/ec2_autostop.py
+++ lambda/ec2_autostop/ec2_autostop.py
@@ -1,3 +1,3 @@
-def lambda_handler():
+def lambda_handler(event, context):
     print("Hello world")
     print("This should be working. IF this is printed, EventBridge rules are good to go, as well as IAM permissions.")
