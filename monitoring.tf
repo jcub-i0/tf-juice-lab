9415a9db2fafab0f382245ed072eb1d92f8d8622
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-07-10 22:01:21 -0400

    Added RENOTIFY_AFTER_HOURS environmnet variable

--- monitoring.tf
+++ monitoring.tf
@@ -283,6 +283,7 @@ resource "aws_lambda_function" "ec2_isolation" {
   environment {
     variables = {
       QUARANTINE_SG_ID = aws_security_group.quarantine_sg.id
+      RENOTIFY_AFTER_HOURS = var.renotify_after_hours_isolate
     }
   }
 
@@ -347,7 +348,7 @@ resource "aws_lambda_function" "ec2_autostop" {
       IDLE_CPU_THRESHOLD  = var.idle_cpu_threshold
       IDLE_PERIOD_MINUTES = var.idle_period_minutes
       SNS_TOPIC_ARN       = aws_sns_topic.alerts.arn
-      RENOTIFY_AFTER_HOURS = var.renotify_after_hours
+      RENOTIFY_AFTER_HOURS = var.renotify_after_hours_autostop
     }
   }
 }
