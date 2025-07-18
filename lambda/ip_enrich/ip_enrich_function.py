573bfa7bee58c636b2992a391d62a31bee5a0ee6
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-07-18 16:05:48 -0400

    Finished IP Enrich Lambda func (for now); will add rate limiting later

--- lambda/ip_enrich/ip_enrich_function.py
+++ lambda/ip_enrich/ip_enrich_function.py
@@ -6,6 +6,7 @@ import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+sns = boto3.client('sns')
 
 
 ABUSE_IPDB_API_KEY = os.environ.get('ABUSE_IPDB_API_KEY')
@@ -25,7 +26,7 @@ def extract_ip(finding):
 
     return ips
 
-# Query AbuseIPDP for IP address data
+# Query AbuseIPDB for IP address data
 def query_abuse_ipdb(ip):
     url = 'https://api.abuseipdb.com/api/v2/check'
 
@@ -46,14 +47,75 @@ def query_abuse_ipdb(ip):
         return data.get('data', {})
     
     except Exception as e:
-        logging.error(f'Error querying AbuseIPDB for IP {ip}')
+        logger.error(f'Error querying AbuseIPDB for IP {ip}: {str(e)}')
         return None
 
+def publish_to_alerts_sns(data):
+    if not SNS_TOPIC_ARN:
+        logger.warning(f'SNS topic not set. Skipping alert notification.')
+        return
+    
+    message = json.dumps(data, indent=2)
+
+    try:
+        sns.publish(
+            TopicArn = SNS_TOPIC_ARN,
+            Subject = 'AbuseIPDB Lookup Results',
+            Message = message
+        )
+        logger.info('SNS notification sent.')
+
+    except Exception as e:
+        logger.error(f'Failed to publish to SNS: {str(e)}')
+
 def lambda_handler(event, context):
+    data = []
+    all_ips = set()
+    ip_to_finding_map = {}
     findings = event['detail']['findings']
 
+    if not findings:
+        logger.warning(f'No findings found in event.')
+        return {'statusCode': 400, 'body': json.dumps({'message': 'No findings in event'})}
+
     for finding in findings:
+        finding_id = finding.get('Id','Unknown')
         ips = extract_ip(finding)
+        for ip in ips:
+            ip_to_finding_map[ip] = finding_id
+
+        all_ips.update(ips)
+
+    logger.info(f'Total unique IPs extracted: {len(all_ips)}')
+
+    for ip in all_ips:
+        result = query_abuse_ipdb(ip)
+
+        if result:
+            data.append({
+                'findingId': ip_to_finding_map.get(ip),
+                'ip': ip,
+                'intel': {
+                    'abuseIPDB': {
+                        'abuseConfidenceScore': result.get('abuseConfidenceScore'),
+                        'countryName': result.get('countryName'),
+                        'usageType': result.get('usageType'),
+                        'domain': result.get('domain'),
+                        'hostnames': result.get('hostnames'),
+                        'isp': result.get('isp'),
+                        'isTor': result.get('isTor'),
+                        'lastReportedAt': result.get('lastReportedAt')
+                    }
+
+                }
+            })
+
+    if data:
+        message = json.dumps(data, indent=2)
+        publish_to_alerts_sns(data)
 
 
-    return None
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'Processing complete', 'resultCount': len(data)})
+    }
