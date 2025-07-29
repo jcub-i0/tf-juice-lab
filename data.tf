68fc757ea6609ff363c560d6a2cef182f7591b4b
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-07-29 00:10:21 -0400

    Added condition block to aws_iam_policy_document.config_permissions to only allow SourceAccount access

--- data.tf
+++ data.tf
@@ -73,6 +73,11 @@ data "aws_iam_policy_document" "config_permissions" {
       "lambda:Get*"
     ]
     resources = ["*"]
+    condition {
+      test = "StringEquals"
+      variable = "aws:SourceAccount"
+      values = [data.aws_caller_identity.current.account_id]
+    }
   }
 }
 
