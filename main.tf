60451cec4269529d6b8ce03d5d2047bf0df95d91
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-08-07 19:04:15 -0400

    Created Lambda subnet's route table and associated it with the subnet

--- main.tf
+++ main.tf
@@ -109,6 +109,18 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table" "lambda" {
+  vpc_id = aws_vpc.tf-juice-lab.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+  }
+  tags = {
+    Name = "Lambda RT"
+  }
+}
+
 resource "aws_route_table_association" "private_assc" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
@@ -117,6 +129,11 @@ resource "aws_route_table_association" "private_assc" {
 resource "aws_route_table_association" "public_assc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "lambda_assc" {
+  subnet_id = aws_subnet.lambda_private.id
+  route_table_id = aws_route_table.lambda.id
 }
 
 # CREATE SECURITY GROUPS
