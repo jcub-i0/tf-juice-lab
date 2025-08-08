20edd7f57060fdbe27cb6bf9db4a1b2eb19b847f
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-08-07 22:11:41 -0400

    Created file with vpc endpoints for ec2 isolation lambda

---
+++ vpc_endpoints.tf
@@ -0,0 +1,54 @@
+
+
+# Interface VPC Endpoints for EC2 Isolation Lambda function's needs
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id              = aws_vpc.tf-juice-lab.id
+  service_name        = "com.amazonaws.${var.aws_region}.ec2"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.lambda_private.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "sns" {
+  vpc_id              = aws_vpc.tf-juice-lab.id
+  service_name        = "com.amazonaws.${var.aws_region}.sns"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.lambda_private.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "sqs" {
+  vpc_id              = aws_vpc.tf-juice-lab.id
+  service_name        = "com.amazonaws.${var.aws_region}.sqs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.lambda_private.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "securityhub" {
+  vpc_id              = aws_vpc.tf-juice-lab.id
+  service_name        = "com.amazonaws.${var.aws_region}.securityhub"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.lambda_private.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.tf-juice-lab.id
+  service_name        = "com.amazonaws.${var.aws_region}.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.lambda_private.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.tf-juice-lab.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+}
