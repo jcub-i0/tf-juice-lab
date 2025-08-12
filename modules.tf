5c8989932df87d9078365007f770a0cdf59dbf32
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-08-11 20:55:19 -0400

    Created S3 module for General Purpose Replica S3 bucket

--- modules.tf
+++ modules.tf
@@ -4,7 +4,7 @@ module "kms" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-kms.git?ref=210736c7aaf2394a68e5f85de4e29169ac126363"
 
   aliases             = ["tf-juice-lab"]
-  description         = "KMS key for encrypting CloudTrail, CloudWatch, SNS, SQS"
+  description         = "KMS key for encrypting CloudTrail, CloudWatch, SNS, SQS, Lambda, S3"
   enable_key_rotation = true
 
   key_usage = "ENCRYPT_DECRYPT"
@@ -144,5 +144,42 @@ module "kms" {
   tags = {
     Environment = var.environment
     Project     = "tf-juice-lab"
+  }
+}
+
+# S3 Module for S3 CRR
+module "general_purpose_replica_bucket" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=2a25737a72c7e862ea297cea063207a3aa56b1a8"
+
+  bucket = "general-purpose-replica-${random_id.random_suffix.hex}"
+  region = var.secondary_aws_region
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  /*logging = {
+    target_bucket = aws_s3_bucket.centralized_logs.id
+    target_prefix = "log/general-purpose-replica/"
+  }*/
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  tags = {
+    Name        = "General Purpose Replica"
+    Environment = var.environment
+    Purpose     = "Cross-Region Replication destination bucket"
   }
 }
