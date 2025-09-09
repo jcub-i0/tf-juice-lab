a6042d3c2240e1d990c97dd98657e1965ccac5a6
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-09 00:32:22 -0400

    Updated all references to resources included in the modules/network/ directory

--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -1,80 +1,80 @@
 # Interface VPC Endpoints for EC2 Isolation Lambda function's needs
 resource "aws_vpc_endpoint" "ec2" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.ec2"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "sns" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.sns"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "sqs" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.sqs"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "securityhub" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.securityhub"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "logs" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.logs"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "kms" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.kms"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "cloudtrail" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.cloudtrail"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "monitoring" {
-  vpc_id              = aws_vpc.tf-juice-lab.id
+  vpc_id              = module.network.vpc_id
   service_name        = "com.amazonaws.${var.aws_region}.monitoring"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.lambda_private.id]
+  subnet_ids          = [module.network.lambda_subnet_id]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 }
 
 # Gateway VPC Endpoint
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = aws_vpc.tf-juice-lab.id
+  vpc_id            = module.network.vpc_id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.private.id]
+  route_table_ids   = [module.network.private_route_table_id]
 }
