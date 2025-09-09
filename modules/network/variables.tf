5d02cbfb516f51c92d151c53ec9e08d5b81445c3
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-09 00:36:22 -0400

    Created all necessary variables to be referenced by the module

---
+++ modules/network/variables.tf
@@ -0,0 +1,33 @@
+variable "vpc_cidr" {
+  type    = string
+}
+
+variable "public_sub_cidr" {
+  description = "CIDR address for the Public Subnet"
+  type        = string
+}
+
+variable "public_sub_az" {
+  description = "Availability Zone for the Public Subnet"
+  type        = string
+}
+
+variable "private_sub_cidr" {
+  description = "CIDR address for the Private Subnet"
+  type        = string
+}
+
+variable "private_sub_az" {
+  description = "Availability Zone for the Private Subnet"
+  type        = string
+}
+
+variable "lambda_sub_cidr" {
+  description = "CIDR address for the Lambda Private Subnet"
+  type        = string
+}
+
+variable "lambda_sub_az" {
+  description = "Availability Zone for the Lambda Private Subnet"
+  type        = string
+}
