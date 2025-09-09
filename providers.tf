b749c206681c75fdd703b5d0be587c6c68822962
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-08 23:20:30 -0400

    Moved 'terraform' block and 'provider' blocks from main.tf into providers.tf

---
+++ providers.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = "~>1.12.0"
+
+  backend "s3" {
+    bucket  = "tf-juice-lab-state"
+    key     = "tf-juice-lab-state/tf-state"
+    region  = "us-east-1"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.6.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = var.secondary_aws_region
+}
