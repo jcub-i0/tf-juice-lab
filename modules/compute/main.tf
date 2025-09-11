5a7b9fe6131f73eb481a009aff15e616c7734b8c
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-10 22:37:11 -0400

    Added EC2-related IAM resources

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -338,3 +338,32 @@ EOF
     Name = "JuiceShop"
   }
 }
+
+## Create SSM IAM Role
+resource "aws_iam_role" "ssm_role" {
+  name = "EC2-SSM-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = data.aws_iam_policy.ssm_core.arn
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "EC2-SSM-Profile"
+  role = aws_iam_role.ssm_role.name
+}
