fba7f0178b8faec671134a0ae6d2f6138e4689ce
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-10 23:09:57 -0400

    Added data blocks for AMI lookups

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -186,6 +186,38 @@ resource "aws_key_pair" "juice_key" {
 }
 
 
+data "aws_ami" "amz-linux-2023" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+  owners = ["137112412989"]
+}
+
+# Data blocks for AMI lookups
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+data "aws_ami" "kali-linux" {
+  most_recent = true
+  owners      = ["679593333241"]
+
+  filter {
+    name   = "name"
+    values = ["kali-last-snapshot-amd64-2025.1.4-804fcc46-63fc-4eb6-85a1-50e66d6c7215"]
+  }
+}
+
 # CREATE INSTANCES
 
 resource "aws_instance" "bastion" {
