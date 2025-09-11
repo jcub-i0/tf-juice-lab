33f0a145b4358a34e4fd26607134ceec92d6eb5f
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-10 23:10:51 -0400

    Cut out data blocks for AMI lookups to be placed in module.compute.main

--- data.tf
+++ data.tf
@@ -1,35 +1,5 @@
 # DATA BLOCKS
 
-data "aws_ami" "amz-linux-2023" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-  owners = ["137112412989"]
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
-
-data "aws_ami" "kali-linux" {
-  most_recent = true
-  owners      = ["679593333241"]
-
-  filter {
-    name   = "name"
-    values = ["kali-last-snapshot-amd64-2025.1.4-804fcc46-63fc-4eb6-85a1-50e66d6c7215"]
-  }
-}
-
 data "aws_iam_policy" "ssm_automation" {
   name = "AmazonSSMAutomationRole"
 }
