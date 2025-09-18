3bc800b20753658ce6b0cfd04396578ca0fdef48
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-17 22:59:07 -0400

    Created 'lambda_ec2_isolate_execution_role_arn', 'lambda_autostop_execution_role_arn', and 'lambda_ip_enrich_arn' output resources

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -5,3 +5,15 @@ output "ssm_instance_profile_name" {
 output "replication_role_arn" {
   value = aws_iam_role.replication_role.arn
 }
+
+output "lambda_ec2_isolate_execution_role_arn" {
+  value = aws_iam_role.lambda_ec2_isolate_execution_role.arn
+}
+
+output "lambda_autostop_execution_role_arn" {
+  value = aws_iam_role.lambda_autostop_execution_role.arn
+}
+
+output "lambda_ip_enrich_arn" {
+  value = aws_iam_role.lambda_ip_enrich.arn
+}
