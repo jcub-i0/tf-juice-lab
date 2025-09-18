43beb5257fd6ee8d670076cb1f5bfbd41f165537
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-17 22:38:23 -0400

    Created variables referenced by 'main.tf' in the 'iam' module

---
+++ modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "terraform_admin_username" {
+  description = "IAM username of the Terraform admin user"
+  type        = string
+}
+
+variable "account_id" {
+  description = "The Terraform user's AWS account ID"
+  type        = string
+}
