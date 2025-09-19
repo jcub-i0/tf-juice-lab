b57b00a17d001a4d6a69f94f723a980caf189ecb
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-19 12:44:53 -0400

    Added general_purpose_sns_policy_json output

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -21,3 +21,7 @@ output "lambda_ip_enrich_arn" {
 output "gen_purp_s3_sns_to_sqs_json" {
   value = data.aws_iam_policy_document.gen_purp_s3_sns_to_sqs.json
 }
+
+output "general_purpose_sns_policy_json" {
+  value = data.aws_iam_policy_document.general_purpose_sns_policy.json
+}
