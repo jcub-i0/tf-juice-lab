8dcc4e57331b43d92d5a519bd3899891a667e964
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-19 17:43:58 -0400

    Added 'alerts_sns_topic_arn' variable to 'iam' module

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ module "iam" {
   account_id                 = var.account_id
   general_purpose_bucket_arn = aws_s3_bucket.general_purpose.arn
   kms_key_arn                = module.kms.key_arn
+  alerts_sns_topic_arn       = aws_sns_topic.alerts.arn
 }
 
 resource "aws_security_group" "quarantine_sg" {
