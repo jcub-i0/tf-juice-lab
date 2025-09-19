aa1d5746cf7fef8af63f8f07f43f893df2de9306
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-09-19 17:17:43 -0400

    Added 'kms_key_arn' variable

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -12,3 +12,8 @@ variable "general_purpose_bucket_arn" {
   description = "ARN of the General-Purpose S3 bucket"
   type        = string
 }
+
+variable "kms_key_arn" {
+  description = "ARN of the KMS key used for encryption"
+  type        = string
+}
