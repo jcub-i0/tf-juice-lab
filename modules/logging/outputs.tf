d4edc90ac9943063e7ac19b78a6028872e9fbe5b
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-11-30 22:09:04 -0500

    Added centralized_logs_s3_event_queue_id and centralized_logs_bucket_notifications_arn

--- modules/logging/outputs.tf
+++ modules/logging/outputs.tf
@@ -17,3 +17,11 @@ output "sqs_centralized_logs_event_queue_arn" {
 output "config_delivery_channel" {
   value = aws_config_delivery_channel.config_delivery_channel
 }
+
+output "centralized_logs_s3_event_queue_id" {
+  value = aws_sqs_queue.centralized_logs_s3_event_queue.id
+}
+
+output "centralized_logs_bucket_notifications_arn" {
+  value = aws_sns_topic.centralized_logs_bucket_notifications.arn
+}
