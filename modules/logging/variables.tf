80989028db6c9a7d4eb859d2096f5a5dc4abc6dd
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-12-01 22:59:56 -0500

    Created a bunch of variables and modified those that represent objects' types from 'string' to 'any'

--- modules/logging/variables.tf
+++ modules/logging/variables.tf
@@ -20,7 +20,7 @@ variable "kms_master_key_arn" {
 
 variable "centralized_logs_replica_bucket" {
   description = "Centralized Logs Replica S3 bucket"
-  type        = string
+  type = any
 }
 
 variable "centralized_logs_replica_bucket_arn" {
@@ -28,33 +28,53 @@ variable "centralized_logs_replica_bucket_arn" {
   type        = string
 }
 
+variable "centralized_logs_replica_bucket_s3_bucket_arn" {
+  description = "NOT the same as centralized_logs_replica_bucket_arn -- This is an output from public S3 module"
+  type = string
+}
+
 variable "kms_key_arn" {
   description = "ARN of the KMS key"
   type        = string
 }
 
+variable "kms_replica_secondary_region_key_arn" {
+  description = "ARN of the KMS Replica Key based in the secondary region"
+  type = string
+}
+
 variable "centralized_logs_topic_policy" {
   description = "Centralized Logs Topic Policy"
-  type        = string
+  type        = any
 }
 
 variable "config_configuration_recorder_config_rec" {
-  description = "config_rec attribute of the Configuration Recorder"
-  type        = string
+  description = "Configuration Recorder"
+  type        = any
 }
 
 variable "cloudtrail_to_cw_role" {
   description = "Reference to the aws_iam_role.cloudtrail_to_cw_role resource"
+  type = any
+}
+
+variable "cloudtrail_to_cw_role_arn" {
+  description = "ARN of the aws_iam_role.cloudtrail_to_cw_role resouce"
   type = string
 }
 
 variable "cloudtrail_to_cw_policy" {
   description = "Reference to the aws_iam_role_policy.cloudtrail_to_cw_policy resource"
-  type = string
+  type = any
 }
 
 variable "cloudtrail_logs" {
   description = "Reference to the aws_cloudwatch_log_group.cloudtrail_logs resource"
+  type = any
+}
+
+variable "cloudtrail_logs_arn" {
+  description = "ARN of the aws_cloudwatch_log_group.cloudtrail_logs resource"
   type = string
 }
 
