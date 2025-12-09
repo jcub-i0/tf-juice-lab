aaf6be174a666ad90f8c855999861f80bce25d16
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-12-08 23:50:07 -0500

    Removed aws_region.current data resource - it's only used by the kms module, which already has it defined in modules/kms/data.tf

--- data.tf
+++ data.tf
@@ -2,6 +2,3 @@
 
 # Fetch information about the AWS identity Terraform is currently using
 data "aws_caller_identity" "current" {}
-
-# Fetch details on the current AWS region
-data "aws_region" "current" {}
