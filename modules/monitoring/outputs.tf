c5ed914f4742e7d1bf31e2db49d30082541d456c
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-12-10 18:56:23 -0500

    Created cloudtrail_logs_group, cloudtrail_logs_group_arn, and config_configuration_recorder_config_rec variables

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -9,3 +9,15 @@ output "cloudtrail_log_delivery_arn" {
 output "cloudtrail_notifications_arn" {
   value = aws_sns_topic.cloudtrail_notifications.arn
 }
+
+output "cloudtrail_logs_group" {
+  value = aws_cloudwatch_log_group.cloudtrail_logs
+}
+
+output "cloudtrail_logs_group_arn" {
+  value = aws_cloudwatch_log_group.cloudtrail_logs.arn
+}
+
+output "config_configuration_recorder_config_rec" {
+  value = aws_config_configuration_recorder.config_rec
+}
