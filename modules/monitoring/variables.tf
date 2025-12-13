cbe95ba052c7f37dc9da3d3f9f6130fbfba051ed
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-12-13 13:05:17 -0500

    Created kms_key_arn, vpc_flow_logs_arn, vpc_id, config_role_arn, config_remediation_role_arn, cloudtrail_sns_to_sqs_json, config_delivery_channel, and config_ssm_automation_policy_attachment variables

--- modules/monitoring/variables.tf
+++ modules/monitoring/variables.tf
@@ -17,3 +17,43 @@ variable "guardduty_features" {
   description = "List of features to be added to GuardDuty - Determines data souces"
   type        = list(string)
 }
+
+variable "kms_key_arn" {
+  description = "ARN of the main KMS key"
+  type = string
+}
+
+variable "vpc_flow_logs_arn" {
+  description = "ARN of the VPC Flow Logs resource"
+  type = string
+}
+
+variable "vpc_id" {
+  description = "ID attribute of the main VPC"
+  type = string
+}
+
+variable "config_role_arn" {
+  description = "ARN of the AWS Config IAM role"
+  type = string
+}
+
+variable "config_remediation_role_arn" {
+  description = "ARN of the AWS Config Remediation IAM role"
+  type = string
+}
+
+variable "cloudtrail_sns_to_sqs_json" {
+  description = "JSON attribute of the policy that allows CloudTrail SNS topic to publish to SQS"
+  type = string
+}
+
+variable "config_delivery_channel" {
+  description = "References the config_delivery_channel resource"
+  type = any
+}
+
+variable "config_ssm_automation_policy_attachment" {
+  description = "Config SSM Automation policy attachment resource"
+  type = any
+}
