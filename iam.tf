aca4f735186f0a4fbe01e078892fe76847d5faf6
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-12-14 13:08:49 -0500

    Cut general_purpose_replica_policy resource to be pasted into s3_replication module's main.tf

--- iam.tf
+++ iam.tf
@@ -54,30 +54,3 @@ resource "aws_sqs_queue_policy" "gen_purp_s3_sns_to_sqs" {
   queue_url = module.logging.general_purpose_s3_event_queue_id
   policy    = module.iam.gen_purp_s3_sns_to_sqs_json
 }
-
-resource "aws_s3_bucket_policy" "general_purpose_replica_policy" {
-  provider = aws.secondary
-  bucket   = module.s3_replication.general_purpose_replica_bucket_id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowReplicationRoleWriteToReplica"
-        Effect = "Allow"
-        Principal = {
-          AWS = module.iam.replication_role_arn
-        }
-        Action = [
-          "s3:ReplicateObject",
-          "s3:ReplicateDelete",
-          "s3:ReplicateTags"
-        ]
-        Resource = [
-          module.s3_replication.general_purpose_replica_bucket_arn,
-          "${module.s3_replication.general_purpose_replica_bucket_arn}/*"
-        ]
-      }
-    ]
-  })
-}
