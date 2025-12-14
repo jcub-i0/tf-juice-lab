f317b88798efc875a1b2189f15efcfb897a9b3d9
jcub <64655784+jcub-i0@users.noreply.github.com>
2025-12-14 12:17:51 -0500

    Added resources cut from monitoring.tf

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -265,3 +265,37 @@ resource "aws_sns_topic_policy" "centralized_logs_topic_policy" {
   arn    = var.centralized_logs_bucket_notifications_arn
   policy = var.centralized_logs_topic_policy_json
 }
+
+# General Purpose S3 bucket resources
+# Create SNS topic for S3 event notifications
+resource "aws_sns_topic" "general_purpose_bucket_notifications" {
+  name              = "general-purpose-s3-notifications"
+  kms_master_key_id = var.kms_key_arn
+}
+
+resource "aws_s3_bucket_notification" "general_purpose" {
+  bucket = aws_s3_bucket.general_purpose.bucket
+
+  topic {
+    topic_arn     = aws_sns_topic.general_purpose_bucket_notifications.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "logs/"
+  }
+
+  depends_on = [
+    aws_s3_bucket.general_purpose,
+    aws_sns_topic.general_purpose_bucket_notifications,
+    module.logging.general_purpose_topic_policy
+  ]
+}
+
+resource "aws_sqs_queue" "general_purpose_s3_event_queue" {
+  name              = "general-purpose-s3-events"
+  kms_master_key_id = var.kms_key_arn
+}
+
+resource "aws_sns_topic_subscription" "general_purpose_bucket_notifications_sub" {
+  topic_arn = aws_sns_topic.general_purpose_bucket_notifications.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.general_purpose_s3_event_queue.arn
+}
